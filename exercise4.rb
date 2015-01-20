bbab7f1a87bf7a9618a5bed8c23b1cda0e5ef845
Zachary Toth <zachtoth@gmail.com>
2015-01-20 10:06:21 -0500

    Exercise4 working version

--- exercise4.rb
+++ exercise4.rb
@@ -1,15 +1,10 @@
-one_to_hundred = (1..100)
-
-
-def calc2
-	if one_to_hundred % 3 == 0 && num % 5 == 0
-		puts BitMaker
-	elsif one_to_hundred % 3 == 0 && num % 5 != 0
-		puts Bit
-	elsif one_to_hundred % 5 == 0 && num % 3 != 0
-		puts Maker 
+for one_to_hundred in (1..100)
+	if ((one_to_hundred % 3 == 0) && (one_to_hundred % 5 == 0))
+		puts "BitMaker"
+	elsif ((one_to_hundred % 3 == 0) && (one_to_hundred % 5 != 0))
+		puts "Bit"
+	elsif ((one_to_hundred % 5 == 0) && (one_to_hundred % 3 != 0))
+		puts "Maker" 
 	else puts one_to_hundred
-	end
 end
-
-puts calc2
+end
