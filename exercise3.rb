c0a02ff83fe1489449306f0bf453835d2926987a
Zachary Toth <zachtoth@gmail.com>
2015-01-20 09:58:00 -0500

    Uploading final exercise3 and WIP exercise4

--- exercise3.rb
+++ exercise3.rb
@@ -3,5 +3,9 @@ name = gets.chomp
 puts "And how old are you?"
 age = gets.chomp
 
+# takes in the information and puts it to a variable
+
 puts "Hi #{name}!"
 puts "You were born in #{ 2014 - age.to_i } unless you're birthday has passed, then you were born in #{ 2015 - age.to_i }!"
+
+# prints the name at #{name} and the ages with age.to_i converting age to an interger
---
+++ exercise4.rb
@@ -0,0 +1,15 @@
+one_to_hundred = (1..100)
+
+
+def calc2
+	if one_to_hundred % 3 == 0 && num % 5 == 0
+		puts BitMaker
+	elsif one_to_hundred % 3 == 0 && num % 5 != 0
+		puts Bit
+	elsif one_to_hundred % 5 == 0 && num % 3 != 0
+		puts Maker 
+	else puts one_to_hundred
+	end
+end
+
+puts calc2
