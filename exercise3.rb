53b3e8562e68566d028fe0f3bad13d482b034b17
Zachary Toth <zachtoth@gmail.com>
2015-01-20 12:12:04 -0500

    Updated 3 for birthday

--- exercise3.rb
+++ exercise3.rb
@@ -6,6 +6,6 @@ age = gets.chomp
 # takes in the information and puts it to a variable
 
 puts "Hi #{name}!"
-puts "You were born in #{ 2014 - age.to_i } unless you're birthday has passed, then you were born in #{ 2015 - age.to_i }!"
+puts "You were born in #{ 2014 - age.to_i }, unless your birthday has passed this year in which case you were born in #{ 2015 - age.to_i }!"
 
 # prints the name at #{name} and the ages with age.to_i converting age to an interger
