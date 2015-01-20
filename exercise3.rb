22218b4470f16c724a72bcaa72dcc6d67ed329dd
Zachary Toth <zachtoth@gmail.com>
2015-01-20 09:40:37 -0500

    Added in age option

--- exercise3.rb
+++ exercise3.rb
@@ -1,4 +1,7 @@
 puts "What is your name?"
 name = gets.chomp
+puts "And how old are you?"
+age = gets.chomp
 
 puts "Hi #{name}!"
+puts "You were born in #{ 2014 - age.to_i } unless you're birthday has passed, then you were born in #{ 2015 - age.to_i }!"
