67dfd021902ebf9b5763ebc89fa0dc14e62dabbd
Zachary Toth <zachtoth@gmail.com>
2015-01-20 09:26:57 -0500

    RubyOne: exercise2.rb

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+puts 55 * 0.55
+#  will calculate 15% of the 55 value.
+
+
+puts 5.to_s + "6"
+# .to_s will convert the interger to a string and print out 56 on screen
+
+a = 45628
+b = 7839
+
+puts "The answer of 45628 * 7839 = #{ a * b }"
+
+#the string interpolation #{ a * b } will put the result to the terminal of a * b
+
+# the output will be true due to !(false && false) = true and the || will output true if one statement is true
+
+puts (true && false) || (false && false) || !(false && false)
+
